ea56b539bb97435eb421a1da1922fdcac61a81c6
Евгений <4brain.ru@gmail.com>
2025-08-20 01:45:35 +0300

    TEXT ana TOPICS out of html

---
+++ js/texts.js
@@ -0,0 +1,62 @@
+window.TEXTS = [
+  {
+    id: 't1',
+    parts: [
+      'На улице стояла ',
+      { id:'t1g1', topic:1, correct:'н', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'ветре', post:'ая' },
+      ' погода, мы ',
+      { id:'t1g2', topic:2, correct:'и', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'плыли' },
+      ' на корабле на ',
+      { id:'t1g3', topic:3, correct:'←', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'не', post:'обитаемый' },
+      ' остров.'
+    ]
+  },
+  {
+    id: 't2',
+    parts: [
+      'В музее я увидел искусно ',
+      { id:'t2g1', topic:1, correct:'нн', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'выреза', post:'ую' },
+      ' из дерева фигурку, которая стояла на ',
+      { id:'t2g2', topic:2, correct:'е', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'высоком' },
+      ' постаменте, скрытом в ',
+      { id:'t2g3', topic:3, correct:'←', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'не', post:'освещённом' },
+      ' углу.'
+    ]
+  },
+  {
+    id: 't3',
+    parts: [
+      'На поляне мы нашли ',
+      { id:'t3g1', topic:1, correct:'нн', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'заброше', post:'ый' },
+      ' домик, к которому вела ',
+      { id:'t3g2', topic:2, correct:'е', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'длинная' },
+      ' тропинка, затерявшаяся в ',
+      { id:'t3g3', topic:3, correct:'←', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'не', post:'кошеной' },
+      ' траве.'
+    ]
+  },
+  {
+    id: 't4',
+    parts: [
+      'Художник писал ',
+      { id:'t4g1', topic:1, correct:'нн', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'вдохнове', post:'ые' },
+      ' картины, передававшие ',
+      { id:'t4g2', topic:2, correct:'е', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'восходную' },
+      ' красоту гор, утопающих в ',
+      { id:'t4g3', topic:3, correct:'←', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'не', post:'проходимых' },
+      ' лесах.'
+    ]
+  },
+  {
+    id: 't5',
+    parts: [
+      'Экспедиция наткнулась на ',
+      { id:'t5g1', topic:1, correct:'нн', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'полуразруше', post:'ую' },
+      ' крепость, спрятавшуюся за ',
+      { id:'t5g2', topic:2, correct:'е', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'высокими' },
+      ' скалами, в ',
+      { id:'t5g3', topic:3, correct:'←', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'не', post:'доступном' },
+      ' ущелье.'
+    ]
+  }
+];
---
+++ js/topics.js
@@ -0,0 +1,11 @@
+window.TOPICS = {
+  1: { title: 'Н и НН в суффиксах',
+       ruleUrl: '/rules/topic-1.html',
+       ruleHtml: '<iframe class="rule-frame" src="rules/topic-1.html" loading="lazy"></iframe>' },
+  2: { title: 'Приставки ПРЕ и ПРИ',
+       ruleUrl: '/rules/topic-2.html',
+       ruleHtml: '<iframe class="rule-frame" src="rules/topic-2.html" loading="lazy"></iframe>' },
+  3: { title: 'НЕ слитно и раздельно',
+       ruleUrl: '/rules/topic-3.html',
+       ruleHtml: '<iframe class="rule-frame" src="rules/topic-3.html" loading="lazy"></iframe>' },
+};
