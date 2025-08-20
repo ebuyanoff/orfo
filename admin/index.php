302d6bf94f9c44fc5f0b300122dbce4bbdbdbf35
Евгений <4brain.ru@gmail.com>
2025-08-20 19:13:25 +0300

    admin styles

--- admin/index.php
+++ admin/index.php
@@ -174,7 +174,7 @@ input[type="text"], input[type="date"]{padding:.3rem .4rem;border:0;}
 .tg-input.error{border-color:#c62828; box-shadow:0 0 0 2px rgba(198,40,40,.15)}
 .topics p {font-size:12px; line-height:1.4; margin:0; padding:0; display:inline-block; max-width:200px; overflow:hidden; text-overflow:ellipsis; white-space:nowrap;}
 .topics p a {color:#333; text-decoration:none;}
-::placeholder {color:#999;opacity:0.2;}
+table ::placeholder {color:#999;opacity:0.2;}
 </style>
 
 <h2>Результаты тестов</h2>
