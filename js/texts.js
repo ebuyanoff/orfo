be0650073f2ded7a97b410b6c71d621c12bf98e5
Евгений <4brain.ru@gmail.com>
2025-08-22 20:20:07 +0300

    all texts

--- js/texts.js
+++ js/texts.js
@@ -1,62 +1,226 @@
 window.TEXTS = [
-  {
-    id: 't1',
-    parts: [
-      'На улице стояла ',
-      { id:'t1g1', topic:1, correct:'н', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'ветре', post:'ая' },
-      ' погода, мы ',
-      { id:'t1g2', topic:2, correct:'и', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'плыли' },
-      ' на корабле на ',
-      { id:'t1g3', topic:3, correct:'←', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'не', post:'обитаемый' },
-      ' остров.'
-    ]
-  },
-  {
-    id: 't2',
-    parts: [
-      'В музее я увидел искусно ',
-      { id:'t2g1', topic:1, correct:'нн', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'выреза', post:'ую' },
-      ' из дерева фигурку, которая стояла на ',
-      { id:'t2g2', topic:2, correct:'е', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'высоком' },
-      ' постаменте, скрытом в ',
-      { id:'t2g3', topic:3, correct:'←', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'не', post:'освещённом' },
-      ' углу.'
-    ]
-  },
-  {
-    id: 't3',
-    parts: [
-      'На поляне мы нашли ',
-      { id:'t3g1', topic:1, correct:'нн', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'заброше', post:'ый' },
-      ' домик, к которому вела ',
-      { id:'t3g2', topic:2, correct:'е', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'длинная' },
-      ' тропинка, затерявшаяся в ',
-      { id:'t3g3', topic:3, correct:'←', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'не', post:'кошеной' },
-      ' траве.'
-    ]
-  },
-  {
-    id: 't4',
-    parts: [
-      'Художник писал ',
-      { id:'t4g1', topic:1, correct:'нн', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'вдохнове', post:'ые' },
-      ' картины, передававшие ',
-      { id:'t4g2', topic:2, correct:'е', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'восходную' },
-      ' красоту гор, утопающих в ',
-      { id:'t4g3', topic:3, correct:'←', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'не', post:'проходимых' },
-      ' лесах.'
-    ]
-  },
-  {
-    id: 't5',
-    parts: [
-      'Экспедиция наткнулась на ',
-      { id:'t5g1', topic:1, correct:'нн', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'полуразруше', post:'ую' },
-      ' крепость, спрятавшуюся за ',
-      { id:'t5g2', topic:2, correct:'е', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'высокими' },
-      ' скалами, в ',
-      { id:'t5g3', topic:3, correct:'←', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'не', post:'доступном' },
-      ' ущелье.'
-    ]
-  }
+
+{
+  id: 't1',
+  parts: [
+    '',
+    { id:'t1g1', topic:3, correct:'и', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'Н', post:'' },
+    ' свет ни заря мы пришли на созвон; ',
+    { id:'t1g2', topic:5, correct:'-', left:'-', right:'←', btnLeft:'-', btnRight:'слитно', pre:'по', post:'прежнему' },
+    ' спокойно, а по ',
+    { id:'t1g3', topic:11, correct:'е', left:'у', right:'е', btnLeft:'у', btnRight:'е', pre:'приезд', post:'' },
+    ' в офис менеджер напомнил про ',
+    { id:'t1g4', topic:2, correct:'и', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'оритет' },
+    ' задач.'
+  ]
+},
+
+{
+  id: 't2',
+  parts: [
+    'Она воспита',
+    { id:'t2g1', topic:1, correct:'н', left:'нн', right:'н', btnLeft:'нн', btnRight:'н', pre:'', post:'а' },
+    ' и, как ',
+    { id:'t2g2', topic:12, correct:'г', left:'г', right:'гг', btnLeft:'г', btnRight:'гг', pre:'бло', post:'ер' },
+    ' отдела, открыла окно насте',
+    { id:'t2g3', topic:7, correct:'жь', left:'ж', right:'жь', btnLeft:'ж', btnRight:'жь', pre:'', post:'' },
+    ', но отчёт — это, увы, ',
+    { id:'t2g4', topic:4, correct:'←', left:'←', right:'-', btnLeft:'слитно', btnRight:'-', pre:'полу', post:'правда' },
+    '.'
+  ]
+},
+
+{
+  id: 't3',
+  parts: [
+    'По дороге в Цариц',
+    { id:'t3g1', topic:6, correct:'ы', left:'ы', right:'и', btnLeft:'ы', btnRight:'и', pre:'', post:'но' },
+    ' сломалась ле',
+    { id:'t3g2', topic:0, correct:'ст', left:'ст', right:'с', btnLeft:'ст', btnRight:'с', pre:'', post:'ница' },
+    ' метро, и даже премьер',
+    { id:'t3g3', topic:5, correct:'-', left:'-', right:'←', btnLeft:'-', btnRight:'слитно', pre:'', post:'министр' },
+    ' в новостях не смог отвлечь — в офисе ждёт новый тренаж',
+    { id:'t3g4', topic:10, correct:'ё', left:'о', right:'ё', btnLeft:'о', btnRight:'ё', pre:'', post:'р' },
+    ' для командного фитнеса.'
+  ]
+},
+
+{
+  id: 't4',
+  parts: [
+    'В рабочем чате спорили про удачное соч',
+    { id:'t4g1', topic:9, correct:'е', left:'е', right:'и', btnLeft:'е', btnRight:'и', pre:'', post:'тание' },
+    ' шрифтов: ',
+    { id:'t4g2', topic:11, correct:'_', left:'_', right:'←', btnLeft:'раздельно', btnRight:'слитно', pre:'в', post:'течение' },
+    ' дня пришло ',
+    { id:'t4g3', topic:8, correct:'ъ', left:'ъ', right:'ь', btnLeft:'ъ', btnRight:'ь', pre:'об', post:'яснение' },
+    ' от дизайнера — гайдлайн в ',
+    { id:'t4g4', topic:13, correct:'4', left:'4-х', right:'4', btnLeft:'4-х', btnRight:'4', pre:'', post:'' },
+    ' томах.'
+  ]
+},
+
+{
+  id: 't5',
+  parts: [
+    'Запустить рекламную ',
+    { id:'t5g1', topic:12, correct:'а', left:'о', right:'а', btnLeft:'о', btnRight:'а', pre:'к', post:'мпанию' },
+    ' оказалось вовсе ',
+    { id:'t5g2', topic:3, correct:'_', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'не', post:'сложно' },
+    ': осталось ',
+    { id:'t5g3', topic:2, correct:'е', left:'е', right:'и', btnLeft:'е', btnRight:'и', pre:'пр', post:'одолеть' },
+    ' страх перед ',
+    { id:'t5g4', topic:5, correct:'-', left:'←', right:'-', btnLeft:'слитно', btnRight:'-', pre:'бледно', post:'розовым' },
+    ' лого.'
+  ]
+},
+
+{
+  id: 't6',
+  parts: [
+    'До дедлайна остались сутки, а время теч',
+    { id:'t6g1', topic:10, correct:'ё', left:'ё', right:'о', btnLeft:'ё', btnRight:'о', pre:'', post:'т' },
+    ' и мы на ',
+    { id:'t6g2', topic:4, correct:'←', left:'←', right:'-', btnLeft:'слитно', btnRight:'-', pre:'пол', post:'пути' },
+    '; хорошо, что отношения ',
+    { id:'t6g3', topic:7, correct:'т', left:'т', right:'ть', btnLeft:'т', btnRight:'ть', pre:'строя', post:'ся' },
+    ' на доверии — клиент согласился не ',
+    { id:'t6g4', topic:6, correct:'и', left:'ы', right:'и', btnLeft:'ы', btnRight:'и', pre:'вз', post:'имать' },
+    ' пени.'
+  ]
+},
+
+{
+  id: 't7',
+  parts: [
+    'Ветре',
+    { id:'t7g1', topic:1, correct:'н', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'', post:'ая' },
+    ' погода гнала нас ',
+    { id:'t7g2', topic:11, correct:'←', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'на', post:'встречу' },
+    ' кофе, ведь к концу спринта ',
+    { id:'t7g3', topic:9, correct:'о', left:'а', right:'о', btnLeft:'а', btnRight:'о', pre:'выг', post:'рание' },
+    ' на ',
+    { id:'t7g4', topic:13, correct:'ть', left:'т', right:'ть', btnLeft:'т', btnRight:'ть', pre:'девят', post:'надцать' },
+    ' процентов.'
+  ]
+},
+
+{
+  id: 't8',
+  parts: [
+    'Рыночная кон',
+    { id:'t8g1', topic:8, correct:'ъ', left:'ъ', right:'ь', btnLeft:'ъ', btnRight:'ь', pre:'', post:'юнктура' },
+    ' снова повернулась так, что ',
+    { id:'t8g2', topic:2, correct:'е', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'двидеть' },
+    ' её было ',
+    { id:'t8g3', topic:0, correct:'ч', left:'ч', right:'чь', btnLeft:'ч', btnRight:'чь', pre:'ску', post:'но' },
+    ' даже аналитикам, — они пошли прогуляться по ',
+    { id:'t8g4', topic:12, correct:'о', left:'о', right:'а', btnLeft:'о', btnRight:'а', pre:'тр', post:'туару' },
+    ' вокруг офиса.'
+  ]
+},
+
+{
+  id: 't9',
+  parts: [
+    'Наш KPI — вечно',
+    { id:'t9g1', topic:5, correct:'←', left:'←', right:'-', btnLeft:'слитно', btnRight:'-', pre:'', post:'зелёный' },
+    ' график роста, который ',
+    { id:'t9g2', topic:11, correct:'_', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'в', post:'сравнении' },
+    ' с отчётом из ',
+    { id:'t9g3', topic:10, correct:'о', left:'о', right:'ё', btnLeft:'о', btnRight:'ё', pre:'офш', post:'р' },
+    '-офиса выглядит как «2 ',
+    { id:'t9g4', topic:6, correct:'ы', left:'и', right:'ы', btnLeft:'и', btnRight:'ы', pre:'столиц', post:'' },
+    '» на карте: красиво, но далековато.'
+  ]
+},
+
+{
+  id: 't10',
+  parts: [
+    'После янва',
+    { id:'t10g1', topic:7, correct:'рь', left:'рь', right:'р', btnLeft:'рь', btnRight:'р', pre:'', post:'ские' },
+    ' празники мы вернулись в офис, а коллега продолжил ',
+    { id:'t10g2', topic:2, correct:'е', left:'и', right:'е', btnLeft:'и', btnRight:'е', pre:'пр', post:'бывать' },
+    ' в командировке — объехал уже ',
+    { id:'t10g3', topic:4, correct:'-', left:'-', right:'_', btnLeft:'-', btnRight:'раздельно', pre:'пол', post:'Китая' },
+    ' и встретил ',
+    { id:'t10g4', topic:12, correct:'м', left:'мм', right:'м', btnLeft:'мм', btnRight:'м', pre:'э', post:'игрант' },
+    '-стартапера.'
+  ]
+},
+
+{
+  id: 't11',
+  parts: [
+    'Наш бэклог — как пл',
+    { id:'t11g1', topic:9, correct:'о', left:'о', right:'а', btnLeft:'о', btnRight:'а', pre:'', post:'вец' },
+    ' на марафоне: гребёт без остановок; между тем на ',
+    { id:'t11g2', topic:1, correct:'н', left:'нн', right:'н', btnLeft:'нн', btnRight:'н', pre:'кожа', post:'ый' },
+    ' диван плюхнулась распечатка из ',
+    { id:'t11g3', topic:8, correct:'н', left:'н', right:'нъ', btnLeft:'н', btnRight:'нъ', pre:'ми', post:'юст' },
+    ': «работа ',
+    { id:'t11g4', topic:3, correct:'_', left:'_', right:'←', btnLeft:'раздельно', btnRight:'слитно', pre:'не', post:'закончена' },
+    '».'
+  ]
+},
+
+{
+  id: 't12',
+  parts: [
+    'Мы любим бл',
+    { id:'t12g1', topic:9, correct:'и', left:'е', right:'и', btnLeft:'е', btnRight:'и', pre:'', post:'стать' },
+    ' результатами, и нельзя ',
+    { id:'t12g2', topic:3, correct:'←', left:'←', right:'_', btnLeft:'слитно', btnRight:'раздельно', pre:'не', post:'дооценить' },
+    ' роль дружной ',
+    { id:'t12g3', topic:10, correct:'ё', left:'о', right:'ё', btnLeft:'о', btnRight:'ё', pre:'ноч', post:'вка' },
+    ' в офисе — формат ',
+    { id:'t12g4', topic:11, correct:'е', left:'е', right:'и', btnLeft:'е', btnRight:'и', pre:'наподоби', post:'' },
+    ' ретрита.'
+  ]
+},
+
+{
+  id: 't13',
+  parts: [
+    'Буд',
+    { id:'t13g1', topic:12, correct:'у', left:'ую', right:'у', btnLeft:'ую', btnRight:'у', pre:'', post:'щий' },
+    ' релиз подтянул ',
+    { id:'t13g2', topic:13, correct:'мь', left:'мь', right:'м', btnLeft:'мь', btnRight:'м', pre:'восе', post:'сот' },
+    ' правок, так что я накинул ',
+    { id:'t13g3', topic:1, correct:'н', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'вяза', post:'ый' },
+    ' свитер и пошёл ',
+    { id:'t13g4', topic:8, correct:'ы', left:'ы', right:'ъи', btnLeft:'ы', btnRight:'ъи', pre:'под', post:'тожить' },
+    ' всё на стендапе.'
+  ]
+},
+
+{
+  id: 't14',
+  parts: [
+    'Эта метрика ',
+    { id:'t14g1', topic:9, correct:'а', left:'о', right:'а', btnLeft:'о', btnRight:'а', pre:'к', post:'сается' },
+    ' всех, даже ',
+    { id:'t14g2', topic:6, correct:'ы', left:'ы', right:'и', btnLeft:'ы', btnRight:'и', pre:'бледнолиц', post:'й' },
+    ' стажёр, который ',
+    { id:'t14g3', topic:7, correct:'нь', left:'н', right:'нь', btnLeft:'н', btnRight:'нь', pre:'ня', post:'читься' },
+    ', заслужил ',
+    { id:'t14g4', topic:4, correct:'-', left:'-', right:'←', btnLeft:'-', btnRight:'слитно', pre:'пол', post:'арбуза' },
+    ' из фруктовой тарелки.'
+  ]
+},
+
+{
+  id: 't15',
+  parts: [
+    'В корпоративном чате обсуждали прида',
+    { id:'t15g1', topic:1, correct:'н', left:'н', right:'нн', btnLeft:'н', btnRight:'нн', pre:'', post:'ое' },
+    ' открытого офиса — растения и диваны; ',
+    { id:'t15g2', topic:12, correct:'ую', left:'ую', right:'у', btnLeft:'ую', btnRight:'у', pre:'след', post:'щий' },
+    ' бюджет включает более ',
+    { id:'t15g3', topic:13, correct:'сот', left:'ста', right:'сот', btnLeft:'ста', btnRight:'сот', pre:'двух', post:'' },
+    ' лампочек.'
+  ]
+}
+
 ];
